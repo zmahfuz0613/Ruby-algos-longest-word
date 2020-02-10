a7667f6f7282f7c6bfb386dc2855ddfd47412603
mickmed <mick2090@gmail.com>
2020-02-10 16:49:15 -0500

    init

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,11 +1,11 @@
-let algos = require("../groupArrays.js");
+let algos = require("../longestWord.js");
 
-test("groups the arrays into an array of arrays of given length, any remainding go in last array", () => {
-    
-    expect(algos.groupArrays(["a", "b", "c", "d"], 4)).toMatchObject([ [ 'a', 'b', 'c', 'd' ] ])
-    expect(algos.groupArrays(["a", "b", "c", "d"], 3)).toMatchObject([ [ 'a', 'b', 'c' ], [ 'd' ] ])
-    expect(algos.groupArrays(["a", "b", "c", "d"], 2)).toMatchObject([ [ 'a', 'b' ], [ 'c', 'd' ] ])
-    
-  })
+//LONGEST WORD
+//return the longest word in a given array
+test("finds the longest word in the array", () => {
+  expect(
+    algos.longestWord("The quick brown fox jumped over the lazy dog")
+  ).toBe("jumped")
+})
 
   
---
+++ longestWord.js
@@ -0,0 +1,17 @@
+//LONGEST WORD
+//return the longest word in a given array
+const longestWord = str => {
+  var words = str.split(" ")
+  var longest = ""
+  for (var word of words) {
+    if (word.length > longest.length) longest = word
+  }
+  return longest
+  //return str.split(" ").sort(function(a, b) {return b.length - a.length})[0].length
+}
+console.log(longestWord("The quick brown fox jumped over the lazy dog"))
+
+
+module.exports = {
+  longestWord
+};
