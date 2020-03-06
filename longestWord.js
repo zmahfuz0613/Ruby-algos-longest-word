8f922e8bc9991a2ed772132098241724efb9bd2e
mickmed <mick2090@gmail.com>
2020-03-06 13:46:29 -0500

    mods

--- longestWord.js
+++ longestWord.js
@@ -1,13 +1,7 @@
 //LONGEST WORD
 //return the longest word in a given array
 const longestWord = str => {
-  let words = str.split(" ")
-  let longest = ""
-  for (var word of words) {
-    if (word.length > longest.length) longest = word
-  }
-  return longest
-  //return str.split(" ").sort(function(a, b) {return b.length - a.length})[0].length
+ 
 }
 console.log(longestWord("The quick brown fox jumped over the lazy dog"))
 
