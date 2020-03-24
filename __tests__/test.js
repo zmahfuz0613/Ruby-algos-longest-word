6c3d2124cfc2dd464cce867c41520fc1539f681b
Misha Kessler <misha.kessler@generalassemb.ly>
2020-03-24 16:39:36 -0400

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,7 @@
 let algos = require("../longestWord.js");
 
 //LONGEST WORD
-//return the longest word in a given array
+
 test("finds the longest word in the array", () => {
   expect(
     algos.longestWord("The quick brown fox jumped over the lazy dog")
