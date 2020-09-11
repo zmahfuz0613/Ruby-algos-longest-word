9721953df706a80aae767c1e773214b9f8fa2adc
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-09-11 10:33:06 -0400

    Completed

--- longest_word.rb
+++ longest_word.rb
@@ -1,13 +1,9 @@
 # RUBY LONGEST WORD
 
 def longest_word(str)
-
+  str.split(' ').sort! { |a_word, b_word| b_word.length <=> a_word.length }[0]
 end
 
-puts longest_word("The quick brown fox jumped over the lazy dog")
-
-
-
-
+puts longest_word('The quick brown fox jumped over the lazy dog')
 
 # wop
